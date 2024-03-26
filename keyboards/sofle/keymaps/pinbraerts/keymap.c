bf73b84dae2574e862cd97427a4a7654eacbbece
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-03-26 19:59:07 +0300

    add fn and arrow keys

--- keyboards/sofle/keymaps/pinbraerts/keymap.c
+++ keyboards/sofle/keymaps/pinbraerts/keymap.c
@@ -54,6 +54,8 @@ static char const* os_to_string(os_variant_t os) {
 #endif // OS_DETECTION_ENABLE
 
 #define RETRO_TAPPING
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 1
 
 enum custom_keys {
     KC_ESCTL = MT(MOD_LCTL, KC_ESC),
@@ -68,7 +70,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,     KC_W,   KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_EQL,
   KC_ESCTL, KC_A,     KC_S,   KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,
   KC_BACKS, KC_MINUS, KC_Z,   KC_X,    KC_C,    KC_V, KC_MUTE,       XXXXXXX,   KC_B,     KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,
-                 TT(settings), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(game)
+                 TT(settings), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(function)
+),
+
+[function] = LAYOUT(
+  KC_GRV,   KC_F1,   KC_F2,    KC_F3,    KC_F4,    KC_F5,                           KC_F6,     KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_BACKSPACE,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_BACKSLASH,
+  KC_EQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_LEFT,     KC_DOWN,    KC_UP,    KC_LEFT, KC_SCLN,  KC_QUOT,
+  KC_MINUS, KC_ESC, KC_Z,    KC_X,    KC_C,    KC_V, KC_MUTE,       XXXXXXX,   KC_B,     KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,
+                TT(vim), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(game)
 ),
 
 [game] = LAYOUT(
@@ -76,23 +86,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_BACKSLASH,
   KC_EQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_MINUS, KC_ESC, KC_Z,    KC_X,    KC_C,    KC_V, KC_MUTE,       XXXXXXX,   KC_B,     KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,
-                 TT(vim), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(function)
-),
-
-[function] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,     KC_7,    KC_8,    KC_9,    KC_0,  KC_BACKSPACE,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_BACKSLASH,
-  KC_EQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_MINUS, KC_ESC, KC_Z,    KC_X,    KC_C,    KC_V, KC_MUTE,       XXXXXXX,   KC_B,     KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,
-                TT(game), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(settings)
+                 TT(function), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(settings)
 ),
 
 [settings] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,     KC_7,    KC_8,    KC_9,    KC_0,  KC_BACKSPACE,
+  KC_GRV,   KC_F1,   KC_F2,    KC_F3,    KC_F4,    KC_F5,                           KC_F6,     KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_BACKSPACE,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_BACKSLASH,
-  KC_EQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_EQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_LEFT,     KC_DOWN,    KC_UP,    KC_LEFT, KC_SCLN,  KC_QUOT,
   KC_MINUS, KC_ESC, KC_Z,    KC_X,    KC_C,    KC_V, KC_MUTE,       XXXXXXX,   KC_B,     KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,
-            TT(function), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(vim)
+            TT(game), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(vim)
 ),
 
 };
