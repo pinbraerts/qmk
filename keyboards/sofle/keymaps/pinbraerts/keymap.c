aaebd0fa20c9e1fad20501aaa03932f11d8862cf
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-03-26 19:46:37 +0300

    fix equal and plus key

--- keyboards/sofle/keymaps/pinbraerts/keymap.c
+++ keyboards/sofle/keymaps/pinbraerts/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [vim] = LAYOUT(
   KC_GRV,   KC_1,     KC_2,   KC_3,    KC_4,    KC_5,                           KC_6,     KC_7,    KC_8,    KC_9,    KC_0,  KC_BACKSPACE,
-  KC_TAB,   KC_Q,     KC_W,   KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_PEQL,
+  KC_TAB,   KC_Q,     KC_W,   KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_EQL,
   KC_ESCTL, KC_A,     KC_S,   KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,
   KC_BACKS, KC_MINUS, KC_Z,   KC_X,    KC_C,    KC_V, KC_MUTE,       XXXXXXX,   KC_B,     KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,
                  TT(settings), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(game)
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [game] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,     KC_7,    KC_8,    KC_9,    KC_0,  KC_BACKSPACE,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_BACKSLASH,
-  KC_PEQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_EQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_MINUS, KC_ESC, KC_Z,    KC_X,    KC_C,    KC_V, KC_MUTE,       XXXXXXX,   KC_B,     KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,
                  TT(vim), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(function)
 ),
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [function] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,     KC_7,    KC_8,    KC_9,    KC_0,  KC_BACKSPACE,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_BACKSLASH,
-  KC_PEQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_EQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_MINUS, KC_ESC, KC_Z,    KC_X,    KC_C,    KC_V, KC_MUTE,       XXXXXXX,   KC_B,     KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,
                 TT(game), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(settings)
 ),
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [settings] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,     KC_7,    KC_8,    KC_9,    KC_0,  KC_BACKSPACE,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_BACKSLASH,
-  KC_PEQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_EQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_MINUS, KC_ESC, KC_Z,    KC_X,    KC_C,    KC_V, KC_MUTE,       XXXXXXX,   KC_B,     KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,
             TT(function), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(vim)
 ),
