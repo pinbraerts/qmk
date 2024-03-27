56bc2294d63a86ccf4ac58246d4add9f514f67d7
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-03-27 09:57:31 +0300

    fix arrow

--- keyboards/sofle/keymaps/pinbraerts/keymap.c
+++ keyboards/sofle/keymaps/pinbraerts/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [function] = LAYOUT(
   KC_GRV,   KC_F1,   KC_F2,    KC_F3,    KC_F4,    KC_F5,                           KC_F6,     KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_BACKSPACE,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_BACKSLASH,
-  KC_EQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_LEFT,     KC_DOWN,    KC_UP,    KC_LEFT, KC_SCLN,  KC_QUOT,
+  KC_EQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_LEFT,     KC_DOWN,    KC_UP,    KC_RIGHT, KC_SCLN,  KC_QUOT,
   KC_MINUS, KC_ESC, KC_Z,    KC_X,    KC_C,    KC_V, KC_MUTE,       XXXXXXX,   KC_B,     KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,
                 TT(vim), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(game)
 ),
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [settings] = LAYOUT(
   KC_GRV,   KC_F1,   KC_F2,    KC_F3,    KC_F4,    KC_F5,                           KC_F6,     KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_BACKSPACE,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_BACKSLASH,
-  KC_EQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_LEFT,     KC_DOWN,    KC_UP,    KC_LEFT, KC_SCLN,  KC_QUOT,
+  KC_EQL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_LEFT,     KC_DOWN,    KC_UP,    KC_RIGHT, KC_SCLN,  KC_QUOT,
   KC_MINUS, KC_ESC, KC_Z,    KC_X,    KC_C,    KC_V, KC_MUTE,       XXXXXXX,   KC_B,     KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,
             TT(game), KC_LGUI, KC_LALT,  KC_SPC, KC_ESCTL,       KC_RSFT, KC_ENT,  KC_LBRC, KC_RBRC, TT(vim)
 ),
