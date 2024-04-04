e0b75a12609be8caf50637baf6aa8eac93d1dae5
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-04-04 11:19:05 +0300

    shift on backspace

--- keyboards/sofle/keymaps/pinbraerts/keymap.c
+++ keyboards/sofle/keymaps/pinbraerts/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,     KC_W,   KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_MINUS,
   MT(MOD_LCTL, KC_ESC), KC_A,     KC_S,   KC_D,    KC_F,    KC_G,               KC_H,     KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,
   MT(MOD_LSFT, KC_LBRC), KC_RBRC, KC_Z, KC_X,    KC_C,    KC_V, KC_MUTE,       XXXXXXX,   KC_B,     KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,
-  TT(settings), KC_LGUI, KC_LALT,  KC_SPC, KC_LCTL,                          KC_ENT, KC_RSFT, KC_BSPC, MT(MOD_RALT, KC_BACKS), TT(function)
+  TT(settings), KC_LGUI, KC_LALT,  KC_SPC, MT(MOD_LCTL, KC_ESC),                          MT(MOD_RSFT, KC_BACKSPACE), KC_ENT, KC_BSPC, MT(MOD_RALT, KC_BACKS), TT(function)
 ),
 
 [function] = LAYOUT(
